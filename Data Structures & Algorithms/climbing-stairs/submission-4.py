8787cdb888426b75c55697045ce4278e2c1b2a69
Adarsh Rai <adarshrajeshrai1411@gmail.com>
2026-08-17 22:38:10 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2: return n
+
+        dp = [0]*n
+
+        first,second = 1,2
+        for i in range(2,n):
+            third = first+second
+            first=second
+            second=third
+            
+
+        return third
+      
