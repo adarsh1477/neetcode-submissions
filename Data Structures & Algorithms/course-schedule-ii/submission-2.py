e8dfa8a607fa55d12aa7e2716790998095c0fa26
Adarsh Rai <adarshrajeshrai1411@gmail.com>
2026-09-04 15:39:00 -0400

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        res = []
+        visited,visiting = set(),set()
+
+        for u,v in prerequisites:
+            adj[v].append(u)
+
+
+        def dfs(node):
+            if node in visited: return False
+            visiting.add(node)
+
+            for nei in adj[node]:
+                if nei in visiting: return True
+                if dfs(nei): return True
+
+
+            visited.add(node)
+            res.append(node)
+            visiting.remove(node)
+            return False
+
+
+
+        for course in range(numCourses):
+            if dfs(course): return []
+
+        return res[::-1]
